eeb2364a5b90df507e4644e31cf5a035ce5051a8
ben howard <possibly.philip@gmail.com>
2010-10-23 20:00:45 -0700

    this is a running version of Koi that is just a proof of concept for some basic functionality at this point. the README has the GPL tagging as does the Koi source

--- Koi.c
+++ Koi.c
@@ -1,3 +1,21 @@
+/*
+  Koi - a GIMP image authentication plugin
+    Copyright (C) 2010  ben howard
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <libgimp/gimp.h>
 #include <libgimp/gimpui.h>
 
@@ -81,7 +99,7 @@ query (void)
     G_N_ELEMENTS (args), 0,
     args, NULL);
 
-  gimp_plugin_menu_register ("Koi image authentication", "<Image>/Filters/Misc");
+  gimp_plugin_menu_register ("Koi", "<Image>/Filters/Misc");
 }
 
 static void run (const guchar *name, int nparams, const GimpParam *param,  int *nreturn_vals, GimpParam **return_vals)
